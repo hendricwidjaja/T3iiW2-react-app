bfa7fb45ea3293f74774e1edd16fbc247dac0e32
hendricwidjaja <hcwidjaja@gmail.com>
2025-01-25 12:53:15 +1100

    feat: Test cases created for Async API fetch requests

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,61 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useUserAuthContext } from './contexts/UserAuthContextProvider';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [token, setToken] = useUserAuthContext();
 
   useEffect(() => {
     console.log(import.meta.env.VITE_AUTH_API_URL)
   }, []);
 
+  // This should be in its own file/folder
+  const connectToAPI = async () => {
+    // Make API requests to "/" - the default route
+    let response = await fetch(`${import.meta.env.VITE_AUTH_API_URL}/`);
+
+    let data = await response.json();
+    console.log(data);
+  }
+
+  // This should be in its own file/folder
+  const postUserLogin = async () => {
+    let userDetails = {
+      username: "Somename", // Should be retrieved from frontend
+      password: "asdfasdf" // Should be retrieved from frontend
+    };
+
+    let response = await fetch(
+      `${import.meta.env.VITE_AUTH_API_URL}/api/auth/login`,
+      {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userDetails)
+      }
+    );
+
+    let data = await response.json();
+    console.log(data)
+
+    setToken(data.token)
+  }
+
   return (
     <>
       <div>
+        {/* If we get a value here, means we correctly made the API request */}
+        <h1 data-testId="jwt-header">{token}</h1>
+        <button onClick={connectToAPI}>
+          API Connection Check
+        </button>
+        <button data-testId="login-button" onClick={postUserLogin}>
+          Login User
+        </button>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,36 +1,63 @@
-import { test } from "vitest"
-import App from "../src/App"
-import { render, screen } from "@testing-library/react"
+import { test } from "vitest";
+import App from "../src/App";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { UserAuthContextProvider } from "../src/contexts/UserAuthContextProvider";
 
 // render content and check if the rendered content is as expected
 
 test("Render the App component", () => {
-    render(<App />)
+  render(<App />);
 
-    const mainPageHeader = screen.getByText("Vite + React")
-    expect(mainPageHeader).toBeInTheDocument();
+  const mainPageHeader = screen.getByText("Vite + React");
+  expect(mainPageHeader).toBeInTheDocument();
 });
 
 // render content and simulate interactions
 test("Render the App component with a button that increases the count", async () => {
-    render(<App />);
+  render(<App />);
 
-    // Find the button element on the screen
-    const counterButton = screen.getByTestId("counterButton");
+  // Find the button element on the screen
+  const counterButton = screen.getByTestId("counterButton");
 
-    // Check the button's text for "count is 0"
-    expect(counterButton).toBeInTheDocument();
-    expect(counterButton).toHaveTextContent("count is 0");
+  // Check the button's text for "count is 0"
+  expect(counterButton).toBeInTheDocument();
+  expect(counterButton).toHaveTextContent("count is 0");
 
-    // Click on the button
-    
-    // Create a user
-    const user = userEvent.setup();
+  // Click on the button
 
-    // Implement click action on the button
-    await user.click(counterButton);
+  // Create a user
+  const user = userEvent.setup();
 
-    // Check the button's text for "count is 1"
-    expect(counterButton).toHaveTextContent("count is 1")
+  // Implement click action on the button
+  await user.click(counterButton);
+
+  // Check the button's text for "count is 1"
+  expect(counterButton).toHaveTextContent("count is 1");
+});
+
+test("Render the App component with token on display", async () => {
+  render(
+    <UserAuthContextProvider>
+      <App />
+    </UserAuthContextProvider>
+  );
+
+  // Fetch the heading where the JWT is to be displayed
+  let tokenHeading = screen.getByTestId('jwt-header');
+  expect(tokenHeading).toHaveTextContent("");
+
+  // Find the login button
+  let loginButton = screen.getByTestId('login-button');
+
+  // Setup a user
+  const user = userEvent.setup();
+
+  // User clicks on the login button
+  await user.click(loginButton);
+
+  // Check the token heading for the JWT (need to be await so it waits for data from API)
+  await waitFor(() => {
+    expect(tokenHeading).not.toHaveTextContent("");
+  });
 });
